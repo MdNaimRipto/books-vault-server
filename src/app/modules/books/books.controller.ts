e3e4625378f6485e99b33773747db78c964e2d99
MD Naimur Rahman <naimurtsc567@gmail.com>
2023-07-15 10:02:50 +0600

    Get All Books Controller & Router created

--- src/app/modules/books/books.controller.ts
+++ src/app/modules/books/books.controller.ts
@@ -3,11 +3,12 @@ import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
 import { BooksService } from "./books.service";
+import { IBooks } from "./books.interface";
 
 const createNewBook = catchAsync(async (req: Request, res: Response) => {
   const { ...booksData } = req.body;
   const result = await BooksService.createNewBook(booksData);
-  sendResponse(res, {
+  sendResponse<IBooks>(res, {
     success: true,
     statusCode: httpStatus.OK,
     message: "New Book Created Successfully",
@@ -15,6 +16,17 @@ const createNewBook = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllBooks = catchAsync(async (req: Request, res: Response) => {
+  const result = await BooksService.getAllBooks();
+  sendResponse<IBooks[]>(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Books Retrieved Successfully",
+    data: result,
+  });
+});
+
 export const BooksController = {
   createNewBook,
+  getAllBooks,
 };
--- src/app/modules/books/books.router.ts
+++ src/app/modules/books/books.router.ts
@@ -11,4 +11,6 @@ router.post(
   BooksController.createNewBook
 );
 
+router.get("/getAllBooks", BooksController.getAllBooks);
+
 export const BooksRouter = router;
